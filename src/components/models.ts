d526bf91aad93c94ee64f9cc62e5f8158c7eb395
bolokoz <yurifbeck@gmail.com>
2022-08-16 11:36:12 -0400

    first commit

---
+++ src/components/models.ts
@@ -0,0 +1,18 @@
+export interface Todo {
+  id: number;
+  content: string;
+}
+
+export interface Meta {
+  totalCount: number;
+}
+
+export interface FoodRating {
+  id: number;
+  preco: number;
+}
+
+export interface ParmegianaRating {
+  id: number;
+  preco: number;
+}
