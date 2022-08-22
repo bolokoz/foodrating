0120be5fe9ab0ee6f618cfd4755d89d4359056a9
bolokoz <yurifbeck@gmail.com>
2022-08-21 21:34:16 -0400

    fixed images

--- src/pages/ReviewListPage.vue
+++ src/pages/ReviewListPage.vue
@@ -1,87 +1,94 @@
 <template>
-  <q-page padding>
-    <q-card class="my-card" v-for="review in reviews" :key="review.Id">
-      <!-- <q-img v-if="review?.fotos !== null" :src="review?.fotos[0]" /> -->
+  <q-page class="q-pa-md">
+    <q-intersection
+      v-for="review in reviews"
+      :key="review.Id"
+      transition="scale"
+      class="row justify-center q-gutter-xl"
+    >
+      <q-card class="my-card q-ma-sm">
+        <q-carousel
+          v-if="review?.fotos !== null"
+          animated
+          v-model="slide"
+          arrows
+          navigation
+          thumbnails
+          swipeable
+          infinite
+        >
+          <q-carousel-slide
+            v-for="(foto, i) in review.fotos"
+            :key="i"
+            :name="i"
+            :img-src="foto"
+          />
+        </q-carousel>
 
-      <q-carousel
-        v-if="review?.fotos !== null"
-        animated
-        v-model="slide"
-        arrows
-        navigation
-        thumbnails
-        swipeable
-        infinite
-      >
-        <q-carousel-slide
-          v-for="(foto, i) in review.fotos"
-          :key="i"
-          :name="i"
-          :img-src="foto"
-        />
-      </q-carousel>
+        <q-card-section>
+          <q-btn
+            fab
+            color="primary"
+            icon="place"
+            class="absolute"
+            style="top: 0; right: 12px; transform: translateY(-50%)"
+          />
 
-      <q-card-section>
-        <q-btn
-          fab
-          color="primary"
-          icon="place"
-          class="absolute"
-          style="top: 0; right: 12px; transform: translateY(-50%)"
-        />
-
-        <div class="row no-wrap items-center">
-          <div class="col text-h6 ellipsis">{{ review?.prato }}</div>
-          <div
-            class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
-          >
-            <q-icon name="place" />
-            250 ft
+          <div class="row no-wrap items-center">
+            <div class="col text-h6 ellipsis">{{ review?.prato }}</div>
+            <div
+              class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
+            >
+              <q-icon name="place" />
+              250 ft
+            </div>
           </div>
-        </div>
-      </q-card-section>
-      <q-card-section>
-        <div>
-          <span>Nota Sentimental: </span>
-          <q-rating
-            :model-value="review?.nota_sentimental"
-            :max="4"
-            size="1.5em"
-            color="grey"
-            :color-selected="ratingColors"
-          />
-          <span>{{ review?.nota_sentimental }} / 4 : Voltaria pra comer </span>
-        </div>
-        <br />
-        <div>
-          <span>Nota Técnica: </span>
-          <q-rating
-            :model-value="review?.nota_tecnica"
-            :max="5"
-            size="1.5em"
-            color="grey"
-            :color-selected="ratingColors2"
-          />
-          <span>{{ review?.nota_tecnica }} / 5 : Bem feita </span>
-        </div>
-      </q-card-section>
+        </q-card-section>
+        <q-card-section>
+          <div>
+            <span>Nota Sentimental: </span>
+            <q-rating
+              :model-value="review?.nota_sentimental"
+              :max="4"
+              size="1.5em"
+              color="grey"
+              :color-selected="ratingColors"
+            />
+            <span
+              >{{ review?.nota_sentimental }} / 4 : Voltaria pra comer
+            </span>
+          </div>
+          <br />
+          <div>
+            <span>Nota Técnica: </span>
+            <q-rating
+              :model-value="review?.nota_tecnica"
+              :max="5"
+              size="1.5em"
+              color="grey"
+              :color-selected="ratingColors2"
+            />
+            <span>{{ review?.nota_tecnica }} / 5 : Bem feita </span>
+          </div>
+        </q-card-section>
 
-      <q-card-section class="q-pt-none">
-        <div class="text-subtitle1">
-          R$ {{ review.valor }}・{{ review.restaurant[0]?.nome }}
-        </div>
-        <div class="text-caption text-grey">
-          Small plates, salads & sandwiches in an intimate setting.
-        </div>
-      </q-card-section>
+        <q-card-section class="q-pt-none">
+          <div class="text-subtitle1">
+            R$ {{ review.valor }}・{{ review.restaurant[0]?.nome }}
+          </div>
+          <div class="text-caption text-grey">
+            Small plates, salads & sandwiches in an intimate setting.
+          </div>
+        </q-card-section>
 
-      <q-separator />
+        <q-separator />
 
-      <q-card-actions>
-        <q-btn flat round icon="event" />
-        <q-btn flat color="primary"> Reserve </q-btn>
-      </q-card-actions>
-    </q-card>
+        <q-card-actions>
+          <q-btn flat round icon="event" />
+          <q-btn flat color="primary"> Reserve </q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-intersection>
   </q-page>
 </template>
 <script setup>
@@ -115,8 +122,9 @@ function getReviews() {
 }
 </script>
 
-<style lang="sass" scoped>
-.my-card
-  width: 100%
-  max-width: 80wh
+<style scoped>
+.my-card {
+  width: 100%;
+  max-height: 40vh;
+}
 </style>
